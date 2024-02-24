d18106f1d794f9ebe62fe6af38ad1832b42dd3ba
mih <mih@mih.mih>
2024-02-25 01:17:26 +0300

    linux_libx264_cleaned

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -9,43 +9,51 @@ from hachoir.parser import createParser
 
 def get_codec(filepath, channel='v:0'):
     output = check_output(['ffprobe', '-v', 'error', '-select_streams', channel,
-                            '-show_entries', 'stream=codec_name,codec_tag_string', '-of', 
+                            '-show_entries', 'stream=codec_name,codec_tag_string', '-show_entries', 'format=duration', '-of', 
                             'default=nokey=1:noprint_wrappers=1', filepath])
     return output.decode('utf-8').split()
 
-def encode(filepath):
+def encode_photo(filepath):
     basefilepath, extension = os.path.splitext(filepath)
-    output_filepath = basefilepath + '.HEVC' + '.mp4'
+    output_filepath = basefilepath + 'lol' + '.jpg'
     assert(output_filepath != filepath)
-    if os.path.isfile(output_filepath):
-        print('Skipping "{}": file already exists'.format(output_filepath))
-        return None
     print(filepath)
-    # Get the video channel codec
+    call(['ffmpeg', '-i', filepath,] + ['-vf'] + ['hflip'] + [output_filepath]) 
+    os.remove(filepath)
+    return output_filepath    
+
+def encode_video(filepath):
+    basefilepath, extension = os.path.splitext(filepath)
+    if len(extension) > 0:
+       output_filepath = basefilepath + 'lol' + extension # + '.HEVC' + '.mp4'
+    else:
+       output_filepath = basefilepath + 'lol' + '.mp4' # + '.HEVC' + '.mp4' 
+       
+    assert(output_filepath != filepath)
+    print(filepath)
     video_codec = get_codec(filepath, channel='v:0')
-    if video_codec == []:
-        print('Skipping: no video codec reported')
-        return None
-    # Video transcode options
-    if video_codec[0] == 'hevc':
-        if video_codec[1] == 'hvc1':
-            print('Skipping: already h265 / hvc1')
-            return None
-        else:
-            # Copy stream to hvc1
-            video_opts = '-c:v copy -tag:v hvc1'
+    if video_codec == [] :  # music file with
+       video_opts = '' 
+    elif video_codec[0] == 'mjpeg': # it needs for some telegram audio files with covers - they may be saved as video - for ffmpeg static cover image is a video stream
+       video_opts = '-c:v copy'
+
     else:
-        # Transcode to h265 / hvc1
-        video_opts = '-c:v libx265 -crf 28 -tag:v hvc1 -preset fast -threads 8'
-    # Get the audio channel codec
-    audio_codec = get_codec(filepath, channel='a:0')
-    if audio_codec == []:
-        audio_opts = ''
-    elif audio_codec[0] == 'aac':
-        audio_opts = '-c:a copy'
-    else:
-        audio_opts = '-c:a aac -b:a 128k'
-    call(['ffmpeg', '-i', filepath] + video_opts.split() + audio_opts.split() + [output_filepath])
+        
+       video_opts = '-c:v libx264  -vf reverse' # -c:v it is a encode codec. in other platfoms (arm, nvidia, any HW accels) you can and should to experiment foe improving perfomance
+     
+    call(['ffmpeg', '-i', filepath,] + ['-af'] + ['areverse'] + video_opts.split() + [output_filepath]) # add , '-af areverse -vf reverse'
+    os.remove(filepath)
+    return output_filepath
+
+def encode_audio(filepath):
+    basefilepath, extension = os.path.splitext(filepath)
+    output_filepath = basefilepath + 'lol' + '.mp3' # all audio trancsode to mp3. it is fast, you can try any codecs \
+                                                    # you need to change extension ".mp3" here to any, and audiocodec in complex (see next comment and ffmpeg docs)
+    assert(output_filepath != filepath)
+    print(filepath)
+    video_opts = '-c:v copy' 
+    call(['ffmpeg', '-i', filepath,] + ['-af'] + ['areverse'] + ['-c:a'] + ['libmp3lame'] + video_opts.split() + [output_filepath]) # '-c:a libmp3lame' is a mp3 audio 
+                                                                                                                                    #codec. see previous comment for moar
     os.remove(filepath)
     return output_filepath
 
@@ -55,7 +63,7 @@ def get_thumbnail(in_filename, path, ttl):
     try:
         (
             ffmpeg
-            .input(in_filename, ss=ttl)
+            .input(in_filename) #, ss=ttl
             .output(out_filename, vframes=1)
             .overwrite_output()
             .run(capture_stdout=True, capture_stderr=True)
--- bot/helper/utils.py
+++ bot/helper/utils.py
@@ -1,35 +1,103 @@
-import os
+import os, time, re
+from pytube import YouTube
+from pathlib import Path
 from bot import data, download_dir
 from pyrogram.types import Message
-from pyrogram.errors.exceptions.bad_request_400 import MessageNotModified
-from .ffmpeg_utils import encode, get_thumbnail, get_duration, get_width_height
+from .ffmpeg_utils import encode_audio, encode_video, encode_photo, get_thumbnail, get_duration, get_width_height
 
 def on_task_complete():
     del data[0]
-    if len(data) > 0:
-      add_task(data[0])
+    #if len(data) > 0:
+      #add_task(data[0])
 
-def add_task(message: Message):
+def add_task_photo(message: Message):
     try:
-      msg = message.reply_text("```Downloading video...```", quote=True)
+      msg = message.reply_text("dwlding...", quote=True)
       filepath = message.download(file_name=download_dir)
-      msg.edit("```Encoding video...```")
-      new_file = encode(filepath)
+      msg.edit("reversing...")
+      new_file = encode_photo(filepath)
       if new_file:
-        msg.edit("```Video Encoded, getting metadata...```")
-        duration = get_duration(new_file)
-        thumb = get_thumbnail(new_file, download_dir, duration / 4)
-        width, height = get_width_height(new_file)
-        msg.edit("```Uploading video...```")
-        message.reply_video(new_file, quote=True, supports_streaming=True, thumb=thumb, duration=duration, width=width, height=height)
-        os.remove(new_file)
-        os.remove(thumb)
-        msg.edit("```Video Encoded to x265```")
+           width, height = get_width_height(new_file)
+           message.reply_photo(new_file)
+           os.remove(thumb)
+           msg.edit("reversed")
       else:
-        msg.edit("```Something wents wrong while encoding your file. Make sure it is not already in HEVC format.```")
-        os.remove(filepath)
-    except MessageNotModified:
-      pass
+           msg.edit("Something wents wrong while encoding your file")
+           os.remove(filepath)
     except Exception as e:
-      msg.edit(f"```{e}```")
+        
+       msg.edit(f"{e}")
     on_task_complete()
+
+def add_task_video(message: Message):
+    try:
+      msg = message.reply_text("dwlding...", quote=True)
+      filepath = message.download(file_name=download_dir)
+      msg.edit("reversing...")
+      new_file = encode_video(filepath)
+      if new_file:
+           msg.edit("Reversed, I'm making a tumbnail...")
+           duration = get_duration(new_file)
+           thumb = get_thumbnail(new_file, download_dir, duration / 4)
+           width, height = get_width_height(new_file)
+           msg.edit("uploading...")
+           message.reply_video(new_file, quote=True, supports_streaming=True, thumb=thumb, duration=duration, width=width, height=height)
+           os.remove(new_file)
+           os.remove(thumb)
+           msg.edit("reversed")
+      else:
+           msg.edit("Something wents wrong while encoding your file.")
+           os.remove(filepath)
+    except Exception as e:
+
+        msg.edit(f"{e}")
+        
+    on_task_complete()
+
+def add_task_audio(message: Message):
+    try:
+      msg = message.reply_text("dwlding...", quote=True)
+      filepath = message.download(file_name=download_dir)
+      msg.edit("reversing...")
+      new_file = encode_audio(filepath)
+      if new_file:
+           msg.edit("Reversed, I'm making a tumbnail...")
+           duration = get_duration(new_file)
+           thumb = get_thumbnail(new_file, download_dir, duration / 4)
+           width, height = get_width_height(new_file)
+           msg.edit("uploading...")
+           message.reply_video(new_file, quote=True, supports_streaming=True, thumb=thumb, duration=duration, width=width, height=height)
+           os.remove(new_file)
+           os.remove(thumb)
+           msg.edit("reversed")
+      else:
+           msg.edit("Something wents wrong while encoding your file. Make sure it is not already in HEVC format.")
+           os.remove(filepath)
+    except Exception as e:
+
+       msg.edit(f"{e}")
+        
+    on_task_complete()
+
+def add_task_ytvideo(message: Message):
+    try:
+      msg = message.reply_text("dwlding...", quote=True)
+      yt = YouTube(message.text)  # we get youtube link as a str from message
+      filepath = yt.streams.filter(progressive=True, file_extension='mp4', res='360p').order_by('resolution').desc().first().download(download_dir) # 360p may be not presenting on youtube.
+      new_file = encode_video(filepath)
+      if new_file:
+           msg.edit("Reversed, I'm making a tumbnail...")
+           duration = get_duration(new_file)
+           thumb = get_thumbnail(new_file, download_dir, duration / 4)
+           width, height = get_width_height(new_file)
+           msg.edit("uploading...")
+           message.reply_video(new_file, quote=True, supports_streaming=True, thumb=thumb, duration=duration, width=width, height=height)
+           os.remove(new_file)
+           os.remove(thumb)
+           msg.edit("reversed")
+      else:
+           msg.edit("Something wents wrong while encoding your file. Make sure it is not already in HEVC format.")
+           os.remove(filepath)
+    except Exception as e:
+
+        msg.edit(f"{e}")
